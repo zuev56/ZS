ee33e418fe060eda794d71ff7fe402e4cae09037
Зуев Сергей Александрович <s.zuev@komanda-a.ru>
2024-09-25 21:22:23 +0300

    refactor: rename project name and directory

--- VideoStreaming/Zs.RtspToHlsConverter/Program.cs
+++ VideoStreaming/Zs.RtspToHlsConverter/Program.cs
@@ -1,6 +1,6 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Zs.RtspToHlsConverter.Worker;
+using Zs.RtspToHlsConverter;
 
 IHost host = Host.CreateDefaultBuilder(args)
     .ConfigureServices(services =>
--- VideoStreaming/Zs.RtspToHlsConverter/Worker.cs
+++ VideoStreaming/Zs.RtspToHlsConverter/Worker.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace Zs.RtspToHlsConverter.Worker;
+namespace Zs.RtspToHlsConverter;
 
 public sealed class Worker : BackgroundService
 {
