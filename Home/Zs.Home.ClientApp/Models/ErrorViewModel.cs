4b778827bb810d206b10f3120ff5a438bed8c25e
Sergei Zuev <zuev56@ro.ru>
2024-11-30 08:47:31 +0300

    feat: Add Zs.Home.ClientApp project

---
+++ Home/Zs.Home.ClientApp/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Zs.Home.ClientApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
