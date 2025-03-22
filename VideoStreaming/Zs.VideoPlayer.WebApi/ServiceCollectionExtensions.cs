a30010c4a128a9e326ba01089656b67228bca692
Zuev Sergei <zuev56@ro.ru>
2025-03-22 13:38:41 +0300

    chore: remove unused directives

--- VideoStreaming/Zs.VideoPlayer.WebApi/ServiceCollectionExtensions.cs
+++ VideoStreaming/Zs.VideoPlayer.WebApi/ServiceCollectionExtensions.cs
@@ -1,6 +1,5 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Nager.VideoStream;
 using Zs.VideoPlayer.WebApi.Features.Videos;
 
 namespace Zs.VideoPlayer.WebApi;
